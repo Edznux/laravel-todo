3cb1c519375c795ae5779bfa8357eafa93d719ac
Edouard SCHWEISGUTH <edznux@gmail.com>
2015-01-19 00:48:08 +0100

    Orthographe

--- app/controllers/TasksController.php
+++ app/controllers/TasksController.php
@@ -2,37 +2,37 @@
 
 class TasksController extends BaseController {
 
-	//affiche l'index pour les taches
+	//affiche l'index pour les tâches
 	public function index(){
 		$tasks= Task::get();
 		return View::make("home",['tasks'=>$tasks]);
 	}
 
-	// ajouter une tache
+	// ajouter une tâche
 	public function store(){
-		// insert dans la bdd avec uniquement le champ "text", les autre sont par default (id auto incrémenté et "done" = 0 par default)
+		// insert dans la bdd avec uniquement le champ "text", les autres sont par défaut (id auto incrémenté et "done" = 0 par default)
 		$rules = [
 			'text' => 'required|min:8'
 		];
 		$v = Validator::make(Input::get(),$rules);	// on verifie les champs
 
-		if( $v->passes() ){													//gestion des erreur : on insere ou on redirige avec une erreur
+		if( $v->passes() ){								//gestion des erreurs : on insere ou on redirige avec une erreur
 			DB::table('tasks')->insert(["text" => Input::get("text")]);
 		}else{
-			return Redirect::to("/")->withInput()->withErrors($v->errors());	// on re-remplis le champs avec les input précédement rentreé (pas vraiment utile dans ce cas, mais bonne pratique)
+			return Redirect::to("/")->withInput()->withErrors($v->errors());	// on re-remplis le champs avec les inputs précédement rentré (pas vraiment utile dans ce cas, mais bonne pratique)
 		}
 
 		return Redirect::to("/");
 	}
 
-	//update une tache (a faire / fait)
+	//update une tâche (A faire / fait)
 	public function update($id){
 		$task = Task::findOrFail($id);
 		$task->update(["done" => 1]);
 		return Redirect::to("/");
 	}
 
-	// supprime une tâche
+	// supprimer une tâche
 	public function destroy($id){
 		$task = Task::destroy($id);
 		return Redirect::to("/");
